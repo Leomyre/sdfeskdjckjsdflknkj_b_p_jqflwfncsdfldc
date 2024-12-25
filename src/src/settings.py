321848aaf8aa369f5592f64d7e5a793b729e7960
Ghosth <ghleomyre@gmail.com>
2024-12-25 16:37:47 +0300

    pour moi

--- src/src/settings.py
+++ src/src/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-5@6b_-13flwp7+wmgi0^(*^6(yv-a9i9kb3(xjt4@uw8v$gks1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.onrender.com']
+ALLOWED_HOSTS = [
+    'https://backportfolio-leomyre.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    'http://localhost:3000',]
 
 """ ALLOWED_HOSTS = [
     'localhost',
